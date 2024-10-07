6951542ecb7fce076b260773874bc83f6814aa7b
joao-silva09 <joao.e.silva.03@gmail.com>
2024-10-07 11:22:25 -0300

    28 - Validação com Spring Validation.

--- authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserDto.java
@@ -2,6 +2,9 @@ package com.ead.authuser.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.UUID;
@@ -18,15 +21,23 @@ public class UserDto {
 
     private UUID userId;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
 
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Email
     @JsonView(UserView.RegistrationPost.class)
     private String email;
 
+    @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
+    @Size(min = 3, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min = 3, max = 20)
     @JsonView({UserView.PasswordPut.class})
     private String oldPassword;
 
@@ -39,6 +50,7 @@ public class UserDto {
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView({UserView.ImagePut.class})
     private String imageUrl;
 }
