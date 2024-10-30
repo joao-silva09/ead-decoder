fa561bbc1f5b0ff5a1cf7108ce2542935a4f0779
joao-silva09 <joao.e.silva.03@gmail.com>
2024-10-30 11:46:45 -0300

    41 - Filtros Avançados em APIs com Specification Avançado.

--- course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -38,5 +40,6 @@ public class ModuleModel implements Serializable {
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany(mappedBy = "module", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
     private Set<LessonModel> lessons;
 }
